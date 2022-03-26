4d5e20bff0baa52db3227294c76abfbd5bccd897
永东 陈 <CYDHJ@163.COM>
2022-03-26 10:05:44 +0800

    TEST

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using org.apache.rocketmq.client.producer;
+using org.apache.rocketmq.common.message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region producer Message
+            DefaultMQProducer producer = null;
+            try
+            {
+               
+                producer = new DefaultMQProducer("wulangtes");
+
+                 
+                producer.setNamesrvAddr("172.17.22.11:9876");
+                producer.start();
+
+   
+                Message msg = new Message("Go_Ticket_WuLang_Test", "TagA", Encoding.UTF8.GetBytes("RocketMQ  "));
+                SendResult sendResult = producer.send(msg);
+            }
+            finally
+            {
+                producer.shutdown();
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ bulid_MQ.cmd
@@ -0,0 +1,1 @@
+ikvmc -target:library -recurse:*.jar -version:2.0 -out:MQ-NEW.dll
